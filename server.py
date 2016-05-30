b8c07bc8d7c2f4ca24144ea92edb1b7f7812cfb4
girishkumarkh <girishkumarkh@gmail.com>
2016-05-30 02:22:34 +0100

    updated server settings

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask import render_template, jsonify
 # import subprocess
-#some random imports
+# some random imports
 import json
 from datetime import timedelta
 from flask import make_response, request, current_app
@@ -52,30 +52,33 @@ def crossdomain(origin=None, methods=None, headers=None,
         return update_wrapper(wrapped_function, f)
     return decorator
 
+
 @app.route("/")
 def home():
-	with open("playlist.json") as json_file:
-		json_data = json.load(json_file)
-	first = json_data.pop(0)["id"]["videoId"]
-	othervideo = [item["id"]["videoId"] for item in json_data]
-	rest = (',').join(othervideo)
-	return render_template('home.html', first=first, data=rest)  
+    with open("playlist.json") as json_file:
+        json_data = json.load(json_file)
+    first = json_data.pop(0)["id"]["videoId"]
+    othervideo = [item["id"]["videoId"] for item in json_data]
+    rest = (',').join(othervideo)
+    return render_template('home.html', first=first, data=rest)
+
 
 @app.route("/songlist")
 def songlist():
-	with open("data.json") as json_file:
-		json_data = json.load(json_file)
-	return render_template('songlist.html', data=json_data)
+    with open("data.json") as json_file:
+        json_data = json.load(json_file)
+    return render_template('songlist.html', data=json_data)
+
 
 @app.route("/api/toplist")
 @crossdomain(origin='*')
 def api():
-	with open("playlist.json") as json_file:
-		json_data = json.load(json_file)
-	return jsonify(data=json_data)
+    with open("playlist.json") as json_file:
+        json_data = json.load(json_file)
+    return jsonify(data=json_data)
 
 
 if __name__ == "__main__":
-	# subprocess.call(["python worker.py"], shell=True)
-	port = int(os.environ.get("PORT",5000))
-	app.run(host='0.0.0.0', port=port, debug=True)
+    host = os.environ.get('HOST', 'localhost')
+    port = os.environ.get('PORT', 8000)
+    app.run(host=host, port=port, debug=True)
